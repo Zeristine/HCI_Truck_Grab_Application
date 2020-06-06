f2803070cc1cceed1eae392e6f910582cca39aac
Nguyen Van Loi <Loinguyen.hg.n@gmail.com>
2020-06-06 17:42:48 +0700

    add UserLocation

---
+++ hci_truck_summer_2020/lib/screens/userLocationOnMap.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UserLocation extends StatefulWidget {
+
+  @override
+  UserLocationState createState() => UserLocationState();
+}
+
+class UserLocationState extends State<UserLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
