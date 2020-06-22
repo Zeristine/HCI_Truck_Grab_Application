ffd5bbc34888212a0fbee5c2e682f1142c9c23f7
Zeral <tsukaiyo@gmail.com>
2020-06-22 16:36:14 +0700

    [HieuBT] updating

--- hci_truck_summer_2020/lib/models/user.dart
+++ hci_truck_summer_2020/lib/models/user.dart
@@ -2,16 +2,17 @@ class User {
   String id;
   String username;
   String email;
-  String password;
+  String password;  
 
   User(this.id, this.username, this.email, this.password);
 
   toJson() {
     return {
-      "id": id,
-      "username": username,
-      "email": email,
-      "password": password,
+      'id': id,
+      'username': username,
+      'email': email,
+      'password': password,      
     };
-  }
+  }    
+  
 }
