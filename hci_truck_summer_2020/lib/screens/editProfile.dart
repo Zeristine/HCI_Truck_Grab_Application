ee49b9b6824e05c5cca82f178ec5779a48c884ea
Zeral <tsukaiyo@gmail.com>
2020-07-10 17:15:18 +0700

    [HieuBT] Fix typo

--- hci_truck_summer_2020/lib/screens/editProfile.dart
+++ hci_truck_summer_2020/lib/screens/editProfile.dart
@@ -142,7 +142,7 @@ class EditProfileState extends State<EditProfileScreen> {
                         }),
                   ),
                   ListTile(
-                    title: Text('Gai1'),
+                    title: Text('Gái'),
                     leading: Radio(
                         value: 'female',
                         groupValue: genderStr,
