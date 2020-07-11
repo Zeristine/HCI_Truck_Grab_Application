a044210e136441445a0dd8af4f86da4ae30aeada
Zeral <tsukaiyo@gmail.com>
2020-07-11 16:42:05 +0700

    [HieuBT] create DriverRequestDetail and rename DriverQuotationDetail

---
+++ hci_truck_summer_2020/lib/screens/DriverRequestDetail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DriverRequestDetailScreen extends StatefulWidget {
+  @override
+  DriverRequestDetailState createState() => DriverRequestDetailState();
+}
+
+class DriverRequestDetailState extends State<DriverRequestDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
